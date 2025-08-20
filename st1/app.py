7ddf297ffc775228e4c7337ac23a2fa764c2f469
wnsgh0104 <wnsgh9509@gmail.com>
2025-08-20 10:29:11 +0900

    0820

--- st1/app.py
+++ st1/app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.title("Title 입니다.")
-st.write("처음 입니다.")
+page_main = st.Page("main.py", title="Main Page")
+page_1 = st.Page("p1.py", title="Page 1")
+page_2 = st.Page("p2.py", title="Page 2")
+page_3 = st.Page("p3.py", title="Page 3")
+
+page = st.navigation([page_main,page_1,page_2,page_3])
+
+page.run()
